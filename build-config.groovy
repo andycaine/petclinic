9b6f13b7ace39fa89e40b221e958d1c5fa9c59d4
Andy Caine <acaine00@gmail.com>
2012-10-18 22:47:43 +0100

    Added build config

---
+++ build-config.groovy
@@ -0,0 +1,77 @@
+
+
+environments {
+
+  local {
+    tomcat {
+      username = "admin"
+      password = "admin"
+      host = "localhost"
+    }
+
+    db {
+      host = "localhost"
+      dba {
+        username = "root"
+        password = "password"
+      }
+
+    }
+  }
+
+  development {
+
+    tomcat {
+      username = "admin"
+      password = "admin"
+      host = "192.168.33.11"
+    }
+
+    db {
+      host = "192.168.33.11"
+      dba {
+        username = "root"
+        password = "password"
+      }
+
+    }
+
+  }
+
+  test {
+
+    tomcat {
+      username = "admin"
+      password = "admin"
+      host = "192.168.33.12"
+    }
+
+    db {
+      host = "192.168.33.12"
+      dba {
+        username = "root"
+        password = "password"
+      }
+
+    }
+  }
+
+  production {
+
+    tomcat {
+      username = "admin"
+      password = "admin"
+      host = "192.168.33.13"
+    }
+
+    db {
+      host = "192.168.33.13"
+      dba {
+        username = "root"
+        password = "password"
+      }
+
+    }
+  }
+
+}
